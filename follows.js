4dc026802fe7c4ebf932d14d93f245dc83185799
albino2025 <albino2025@users.noreply.github.com>
2014-11-26 18:31:03 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,14 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'Gr5aw0GOFcyEm4IsoAN0VDLJFeHYn9CBd8XOFtE6jxkOD5pC', // Unique master Xively API key to be used as a default
-		key		= 'Gr5aw0GOFcyEm4IsoAN0VDLJFeHYn9CBd8XOFtE6jxkOD5pC',
-		defaultFeeds	= [172663481!Chauffage_Buanderie], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Albino\'s Feed', // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
+	var defaultKey		= '', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+		applicationName	= '', // Replaces Xively logo in the header
+		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
